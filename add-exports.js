7a3b59bd6bc8bd6a5cb2aca635e2a3c81d16ae89
Roy Peled <royp@folloze.com>
2015-11-17 12:26:37 +0200

    added source map fix

--- add-exports.js
+++ add-exports.js
@@ -5,12 +5,13 @@ var fs = require('fs');
 var EXTENSION_FILE = "extension.js";
 var EXTENSION_FF_FILE = "data/extension.js";
 var BACKGROUND_FILE = "background.js";
+var MANIFEST = "manifest.json";
 
 function addExports(file) {
-	console.log("Adding exports to", file);
+    console.log("Adding exports to", file);
 
     try {
-        var stats = fs.lstatSync(file);
+        fs.lstatSync(file);
     } catch(e){
         console.log("can't find", file);
         return;
@@ -21,11 +22,37 @@ function addExports(file) {
     data = "var exports; " + data;
     fs.writeFileSync(file, data, 'utf-8');
     console.log("Success");
-};
+}
+
+function renameSourceMaps(file){
+    console.log("Rename source maps root for", file);
+
+    try {
+        fs.lstatSync(file);
+        fs.lstatSync(MANIFEST);
+    } catch(e){
+        console.log("can't find", file);
+        return;
+    }
+
+    var data = fs.readFileSync(file, 'utf-8');
+    var manifest = fs.readFileSync(MANIFEST, 'utf-8');
+
+    manifest = JSON.parse(manifest);
+
+    var mapRoot = "chrome-extension://" + manifest.key + "/";
+
+    data = data.replace(/sourceMappingURL\=/gi, "sourceMappingURL=" + mapRoot);\
+    fs.writeFileSync(file, data, 'utf-8');
+    console.log("Success");
+}
 
 
 addExports(EXTENSION_FILE);
 addExports(EXTENSION_FF_FILE);
 addExports(BACKGROUND_FILE);
+
+renameSourceMaps(EXTENSION_FILE);
+renameSourceMaps(BACKGROUND_FILE);
 
 
